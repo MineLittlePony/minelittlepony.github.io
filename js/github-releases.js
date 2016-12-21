26e175b9e0b0a4f8c030c13409c0a6124e22928b
Matthew Messinger <mattmess1221@gmail.com>
2016-12-21 14:50:10 -0500

    Add a link to the beta

--- js/github-releases.js
+++ js/github-releases.js
@@ -1,14 +1,45 @@
-function makeReleaseButton() {
-    var URL = "https://api.github.com/repos/MineLittlePony/MineLittlePony/releases/latest"
-    $.get(URL, function (data) {
-        var url = data.html_url
-        var version = data.tag_name
 
-        var mcver = version.substring(0, version.lastIndexOf("."))
-        if (mcver.endsWith(".0"))
-            mcver = mcver.substring(0, mcver.length - 2)
+function getMCVersion(version) {
+    version = version.substring(0, version.lastIndexOf("."))
+    if (version.endsWith(".0"))
+        version = version.substring(0, version.length - 2);
+    return version;
+}
+function makeReleaseButton() {
+    var URL = "https://api.github.com/repos/MineLittlePony/MineLittlePony/releases"
+    $.get(URL, function (data) {
+        var latest;
+        var prerelease
+        for (var i = 0; i < data.length; i++) {
+            var tag = data[i];
+            if (tag.target_commitish == "master") {
+                if (!prerelease && tag.prerelease) {
+                    prerelease = tag;
+                } else if (!tag.prerelease){
+                    latest = tag;
+                    break;
+                }
+            }
+        }
+        var url = latest.html_url
+        var version = latest.tag_name
+
+        var mcver = getMCVersion(version)
 
         $(".mcversion").text(mcver)
         $("#latest").attr("href", url)
+
+        if (prerelease) {
+            var betaUrl = prerelease.html_url
+            var betaVersion = latest.tag_name
+            var betaVer = getMCVersion(betaVersion)
+            if (betaVer != mcver) {
+                $(".mcversion").append(" (Beta: " + betaVer + ")")
+            }
+
+            $("#prerelease").attr("href", betaUrl);
+            $("#prerelease").show()
+            $("#latest").children().first().addClass("download-latest")
+        }
     })
 }
