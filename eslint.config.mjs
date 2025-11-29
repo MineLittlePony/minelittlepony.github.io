a0c1d6284a8afc56c5d4a44dfe4e67ba465a45aa
Ivan Sokolov <keupoz@gmail.com>
2025-11-29 17:00:01 +0700

    Update Tailwind ESLint config

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,7 +1,5 @@
 import antfu from '@antfu/eslint-config'
-import { FlatCompat } from '@eslint/eslintrc'
-
-const compat = new FlatCompat()
+import tailwind from 'eslint-plugin-tailwindcss'
 
 export default antfu({
   formatters: true,
@@ -16,20 +14,27 @@ export default antfu({
     'style/jsx-one-expression-per-line': ['error', { allow: 'non-jsx' }],
   },
   isInEditor: false,
-}, ...compat.config({
-  // https://github.com/francoismassart/eslint-plugin-tailwindcss
-  extends: ['plugin:tailwindcss/recommended'],
-  rules: {
-    'tailwindcss/no-custom-classname': 'off',
-  },
-})).override('antfu/formatter/astro', {
-  languageOptions: {
-    // fix overriding astro parser with plain parser
-    parser: undefined,
-  },
-}).override('antfu/formatter/astro/disables', {
-  rules: {
-    // fix conflicts with formatter
-    'style/multiline-ternary': 'off',
-  },
 })
+  .override('antfu/formatter/astro', {
+    languageOptions: {
+      // fix overriding astro parser with plain parser
+      parser: undefined,
+    },
+  })
+  .override('antfu/formatter/astro', {
+    languageOptions: {
+      // fix overriding astro parser with plain parser
+      parser: undefined,
+    },
+  })
+  .override('antfu/formatter/astro/disables', {
+    rules: {
+      // fix conflicts with formatter
+      'style/multiline-ternary': 'off',
+    },
+  })
+  .append(tailwind.configs['flat/recommended'], {
+    rules: {
+      'tailwindcss/no-custom-classname': 'off',
+    },
+  })
