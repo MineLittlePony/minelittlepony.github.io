853af8d37719af3f4222b11c5633112c09ec09c9
Ivan Sokolov <keupoz@gmail.com>
2025-12-21 19:00:26 +0700

    Fix z-index

--- src/components/ui/color-picker/classes.ts
+++ src/components/ui/color-picker/classes.ts
@@ -1,1 +1,1 @@
-export const ColorPickerThumbClassName = 'size-4 rounded-full border-2 border-white ring-2 ring-zinc-800'
+export const ColorPickerThumbClassName = 'z-1 size-4 rounded-full border-2 border-white ring-2 ring-zinc-800'
