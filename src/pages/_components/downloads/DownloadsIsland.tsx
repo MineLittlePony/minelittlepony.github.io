9b9467efb1f4b965359cec4c16a9015a17b31a8e
Keupoz Falcon <keupoz@gmail.com>
2025-12-26 00:10:09 +0700

    Use Ark UI ClientOnly component for islands

--- src/pages/_components/downloads/DownloadsIsland.tsx
+++ src/pages/_components/downloads/DownloadsIsland.tsx
@@ -1,5 +1,8 @@
-import { createIsland } from '~/components/Island'
+import { ClientOnly } from '@ark-ui/react'
 import { DownloadsApp } from './DownloadsApp'
 import { DownloadsStatic } from './DownloadsStatic'
 
-export const DownloadsIsland = createIsland(DownloadsStatic, DownloadsApp)
+export const DownloadsIsland = ClientOnly.bind(null, {
+  fallback: <DownloadsStatic />,
+  children: <DownloadsApp />,
+})
--- src/pages/tools/_components/ToolsIsland.tsx
+++ src/pages/tools/_components/ToolsIsland.tsx
@@ -1,5 +1,8 @@
-import { createIsland } from '~/components/Island'
+import { ClientOnly } from '@ark-ui/react'
 import { ToolsApp } from './ToolsApp'
 import { ToolsLoader } from './ToolsLoader'
 
-export const ToolsIsland = createIsland(ToolsLoader, ToolsApp)
+export const ToolsIsland = ClientOnly.bind(null, {
+  fallback: <ToolsLoader />,
+  children: <ToolsApp />,
+})
